dd9bf6469c462e65c64ffbfe583829e44386561a
kenshin627 <87zhaoxiaobo@163.com>
2025-10-11 08:09:53 +0800

    csm with aniamtion

--- Engine/core/graphic/shaderSrc/cascadeShadowMap/vs.glsl
+++ Engine/core/graphic/shaderSrc/cascadeShadowMap/vs.glsl
@@ -1,13 +1,39 @@
 #version 460 core
 
+#define MAX_BONE_INFLUENCE 4
+#define MAX_BONE_COUNT 1000
+
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexcoord;
 layout (location = 3) in vec3 aTangent;
+layout (location = 4) in ivec4 aBoneIds;
+layout (location = 5) in vec4  aWeights;
+
+layout (std140, binding = 4) uniform AnimationBuffer
+{
+	mat4 boneMatrices[MAX_BONE_COUNT];
+} animationBuffer;
 
 uniform mat4 modelMatrix;
 
 void main()
 {
-	gl_Position =  modelMatrix * vec4(aPos, 1.0);
+	vec4 animatedPos = vec4(0.0);
+	for(int i = 0; i < MAX_BONE_INFLUENCE; i++)
+	{
+		if(aBoneIds[i] < 0)
+		{
+			continue;
+		}
+		if(aBoneIds[i] >= MAX_BONE_COUNT)
+		{
+			animatedPos = vec4(aPos, 1.0);
+			break;
+		}
+		mat4 boneMat = animationBuffer.boneMatrices[aBoneIds[i]];
+		vec4 localPos = boneMat * vec4(aPos, 1.0);
+		animatedPos += localPos * aWeights[i];
+	}
+	gl_Position =  modelMatrix * animatedPos;
 }
